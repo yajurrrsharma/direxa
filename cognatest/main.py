21ec89aa505ca9875011a751f9e0808682a7f0e1
yajurrrsharma <0411@pacificworldschool.com>
2025-05-06 12:50:26 +0530

    Update main.py

--- cognatest/main.py
+++ cognatest/main.py
@@ -1,13 +1,31 @@
-from datetime import datetime 
+from datetime import datetime
 
 def intro():
-    name=input("Enter your name:")
+
+    '''
+   Taking basic information of the user, crucial for analysis.
+    
+    '''
+
+    name=input("Enter your name: ")
     dob_input = input("Enter your date of birth (DD-MM-YYYY): ")
     date_of_birth = datetime.strptime(dob_input, "%d-%m-%Y").date()
-    print(f"Your date of birth is: {date_of_birth}")
 
     today = datetime.today().date()
     age = today.year - date_of_birth.year - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
     print(f"You are {age} years old.")
 
-intro
+    grade = int(input("Enter your grade: "))
+    city = input("Enter your city: ")
+    
+    if grade > 10:
+        stream = input("Enter your stream: ")
+    
+    else:
+        pref = input("Enter your preferred stream")
+
+    email = input("Enter your email\n")
+
+
+def test():
+    pass
