18ed18a56d22579569b4693e92c210089261e30a
yajurrrsharma <0411@pacificworldschool.com>
2025-05-06 12:28:08 +0530

    Update main.py

--- cognatest/main.py
+++ cognatest/main.py
@@ -1,1 +1,13 @@
+from datetime import datetime 
 
+def intro():
+    name=input("Enter your name:")
+    dob_input = input("Enter your date of birth (DD-MM-YYYY): ")
+    date_of_birth = datetime.strptime(dob_input, "%d-%m-%Y").date()
+    print(f"Your date of birth is: {date_of_birth}")
+
+    today = datetime.today().date()
+    age = today.year - date_of_birth.year - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
+    print(f"You are {age} years old.")
+
+intro
